b78455435d377507805afed44816625360352e14
kev <kev@daryus.com>
2026-06-05 23:30:40 +0100

    removed unwitting load of connection string with OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=localhost,1450;Database=BookStoreDb;...);

--- BookStoreApp.API/Data/BookStoreDbContext.cs
+++ BookStoreApp.API/Data/BookStoreDbContext.cs
@@ -19,9 +19,9 @@ public partial class BookStoreDbContext : IdentityDbContext<ApiUser> //cip...29
 
   public virtual DbSet<Book> Books { get; set; }
 
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-      => optionsBuilder.UseSqlServer("Server=localhost,1450;Database=BookStoreDb;User Id=sa;Password=Str0ngPa$$w0rd;TrustServerCertificate=True;MultipleActiveResultSets=true");
+      //=> optionsBuilder.UseSqlServer("Server=localhost,1450;Database=BookStoreDb;User Id=sa;Password=Str0ngPa$$w0rd;TrustServerCertificate=True;MultipleActiveResultSets=true");
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
